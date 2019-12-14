87c665d4d3ac2e22053d547200fec22410a307bb
Justin <justinh@gmail.com>
2019-12-13 17:13:57 -0700

    tehello

---
+++ src/main/java/com/spring4/Controller/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.spring4.Controller;
+
+import java.util.Date;
+
+public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("constructing...");
+
+    }
+
+    public String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.print("hello:" + name);
+    }
+
+
+}
