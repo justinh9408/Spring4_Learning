23e349fd97c0b1551d7bc57478246130cdce75c7
Justin <justinh@gmail.com>
2019-12-15 19:46:41 -0700

    te

--- src/main/java/com/spring4/Controller/HelloWorld.java
+++ src/main/java/com/spring4/Controller/HelloWorld.java
@@ -1,13 +1,19 @@
 package com.spring4.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
+@Component
 public class HelloWorld {
+
     public HelloWorld() {
         System.out.println("constructing...");
 
     }
-
+    @Value("helllooo")
     public String name;
 
     public void setName(String name) {
@@ -15,7 +21,7 @@ public class HelloWorld {
     }
 
     public void hello() {
-        System.out.print("hello:" + name);
+        System.out.print("hello:"+name);
 
     }
 }
---
+++ src/main/java/com/spring4/Controller/Main.java
@@ -0,0 +1,16 @@
+package com.spring4.Controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //HelloWord he = new HelloWord();
+
+        //1.创建spring的ioc容器对象
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
+        //2.从ioc容器中获取bean实例
+        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld2");
+        helloWorld.hello();
+    }
+}
